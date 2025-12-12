bcf49a108799ba580dd8729ee97bd441416c84d1
Zen <zenmuhammad072@gmail.com>
2025-12-12 12:10:58 +0700

    pengenalan php

---
+++ kondisi.php
@@ -0,0 +1,9 @@
+<?php
+
+$a=45;
+
+if ($a % 2 == 0) {
+    echo "$a adalah Bilangan Genap";
+}else{
+    echo "$a adalah Bilangan Ganjil";
+}
---
+++ variabel.php
@@ -0,0 +1,8 @@
+<?php
+
+$a=5;
+$b=10;
+
+$c=$a+$b;
+
+echo "Hasil $a ditambah $b adalah $c";
